8c3793c53228ba916280e4e6146e6c955d2b0fb5
Alexis Rico <sferadev@gmail.com>
2022-03-11 21:17:34 +0100

    Add util addPathParam

--- plugins/typescript/src/index.ts
+++ plugins/typescript/src/index.ts
@@ -6,3 +6,4 @@ export { generateFetchers } from "./generators/generateFetchers";
 // Utils
 export { renameComponent } from "./utils/renameComponent";
 export { forceReactQueryComponent } from "./utils/forceReactQueryComponent";
+export { addPathParam } from "./utils/addPathParam";
