7ba70a41c58b61c4189bababce105eda8d198a48
Fabien BERNARD <fabien0102@hotmail.com>
2022-03-16 18:18:13 +0100

    Apply @moltar suggestion

--- cli/src/commands/GenerateCommand.ts
+++ cli/src/commands/GenerateCommand.ts
@@ -1,7 +1,7 @@
 import { Command, Option, UsageError } from "clipanion";
 import * as t from "typanion";
 import fsExtra from "fs-extra";
-import _path from "path";
+import { posix as path } from "path";
 import * as swc from "@swc/core";
 import prettier from "prettier";
 import { fileURLToPath } from "url";
@@ -11,7 +11,6 @@ import { Config, FromOptions, Namespace } from "../types";
 import { getOpenAPISourceFile } from "../core/getOpenAPISourceFile.js";
 import { parseOpenAPISourceFile } from "../core/parseOpenAPISourceFile.js";
 
-const path = _path.posix;
 const __filename = fileURLToPath(import.meta.url);
 
 // if no config -> tell the user to do `openapi-codegen init`
