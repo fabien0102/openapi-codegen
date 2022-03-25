e2ee66222b317366648a256b110e57246f19707c
Fabien BERNARD <fabien0102@hotmail.com>
2022-03-25 15:47:03 +0100

    Leaner spinner

--- cli/src/prompts/Spinner.tsx
+++ cli/src/prompts/Spinner.tsx
@@ -1,39 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import type { FC } from "react";
 import { Text } from "ink";
-import spinners from "cli-spinners";
-import type { SpinnerName } from "cli-spinners";
 
-interface Props {
-  /**
-   * Type of a spinner.
-   * See [cli-spinners](https://github.com/sindresorhus/cli-spinners) for available spinners.
-   *
-   * @default dots
-   */
-  type?: SpinnerName;
-}
+const frames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
 /**
- * Spinner.
+ * Spinner
  */
-export const Spinner: FC<Props> = ({ type = "dots" }) => {
+export const Spinner = () => {
   const [frame, setFrame] = useState(0);
-  const spinner = spinners[type];
 
   useEffect(() => {
     const timer = setInterval(() => {
       setFrame((previousFrame) => {
-        const isLastFrame = previousFrame === spinner.frames.length - 1;
+        const isLastFrame = previousFrame === frames.length - 1;
         return isLastFrame ? 0 : previousFrame + 1;
       });
-    }, spinner.interval);
+    }, 80);
 
     return () => {
       clearInterval(timer);
     };
-  }, [spinner]);
+  }, []);
 
-  return <Text color="green">{spinner.frames[frame]} </Text>;
+  return <Text color="green">{frames[frame]} </Text>;
 };
