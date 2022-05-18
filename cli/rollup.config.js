280aa730a6f7997ba978c0e084a78a40989169d9
Fabien BERNARD <fabien0102@hotmail.com>
2022-05-18 16:56:19 +0200

    Bundle cli with react

---
+++ cli/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import shebang from "rollup-plugin-preserve-shebang";
+import autoExternal from "rollup-plugin-auto-external";
+import internal from "rollup-plugin-internal";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/cli.ts",
+  output: {
+    file: "lib/cli.js",
+    format: "es",
+    banner: "#!/usr/bin/env node",
+  },
+  cache: false,
+  plugins: [
+    shebang(),
+    resolve(),
+    typescript({
+      tsconfig: "./tsconfig.package.json",
+      compilerOptions: {
+        outDir: ".",
+        sourceMap: false,
+      },
+    }),
+    commonjs(),
+    json(),
+    autoExternal(),
+    internal(["react", "ink"]),
+  ],
+  external: ["yoga-layout-prebuilt"],
+};
