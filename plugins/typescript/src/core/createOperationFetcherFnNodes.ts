26d7102fbc60fa4200dcf49c8cc8eba43030e0e4
Fabien BERNARD <fabien0102@hotmail.com>
2023-09-16 13:06:33 +0200

    fix: Parse dot case in path params
    Closed #101

--- plugins/typescript/src/core/createOperationFetcherFnNodes.ts
+++ plugins/typescript/src/core/createOperationFetcherFnNodes.ts
@@ -141,4 +141,4 @@ export const createOperationFetcherFnNodes = ({
  * `pet/{pet_id}` -> `pet/{petId}`
  */
 const camelizedPathParams = (url: string) =>
-  url.replace(/\{[\w\d\-_]*\}/g, (match) => `{${camel(match)}}`);
+  url.replace(/\{[\w\d\-_.]*\}/g, (match) => `{${camel(match)}}`);
