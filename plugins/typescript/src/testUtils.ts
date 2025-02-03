f95d185b90980b6fabc73c7af481004ec62000a8
Fabien Bernard <fabien0102@hotmail.com>
2025-02-03 18:19:05 +0100

    Add prettier in the testing flow

--- plugins/typescript/src/testUtils.ts
+++ plugins/typescript/src/testUtils.ts
@@ -1,4 +1,5 @@
 import ts from "typescript";
+import prettier from "prettier";
 
 const sourceFile = ts.createSourceFile("index.ts", "", ts.ScriptTarget.Latest);
 
@@ -12,3 +13,22 @@ const printer = ts.createPrinter({
  */
 export const print = (node: ts.Node) =>
   printer.printNode(ts.EmitHint.Unspecified, node, sourceFile);
+
+/**
+ * Create a mock (like `vi.fn()`) with embedded prettier step.
+ */
+export const createWriteFileMock = () => {
+  const calls: [string, string][] = [];
+  async function fn(file: string, data: string) {
+    calls.push([
+      file,
+      await prettier.format(data, {
+        parser: "babel-ts",
+      }),
+    ]);
+  }
+
+  fn.mock = { calls };
+
+  return fn;
+};
